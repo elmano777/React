237332f503323d6beab17f0a6d4819b2ec158e3f
Rola Diaz <rolandodiazjara@gmail.com>
2023-03-21 22:47:28 -0500

    cambiando http a https

--- src/helpers/getgif.js
+++ src/helpers/getgif.js
@@ -1,5 +1,5 @@
 export const GetGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=wUILR29K4kDvNSAqLq96gqEmKlDyzXAJ&q=${category}&limit=5`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=wUILR29K4kDvNSAqLq96gqEmKlDyzXAJ&q=${category}&limit=5`;
   const rpta = await fetch(url);
   const { data } = await rpta.json();
   const gifs = data.map((Image) => ({
